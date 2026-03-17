c38124ce11734f15649602428c2b183be3474a4c
Lervinnnn <lquek007@e.ntu.edu.sg>
2026-03-17 14:40:35 +0800

    fix: pass correct courseRunId to AssessmentsSection to match DB records

--- components/CourseDetail.tsx
+++ components/CourseDetail.tsx
@@ -1492,7 +1492,7 @@ export const CourseDetail: React.FC = () => {
                                     course={convertedCourse}
                                     userRole={userRole}
                                     developerAssessments={developerAssessments}
-                                    courseRunId={selectedCourse?.courseRunId}
+                                    courseRunId={convertedCourse.courseRunId}
                                     courseId={selectedCourse?.id}
                                     setDeveloperAssessments={setDeveloperAssessments}
                                     handleFileDownload={handleFileDownload}
