1109880bc355e43ff3be6e4a9822c62bc8b16ae8
Dr. Alfred Ang <angch@tertiaryinfotech.com>
2026-05-01 19:05:44 +0800

    feat: serve per-tenant favicon from training_provider.company_logo_url

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/api/favicon.png?size=32" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/api/favicon.png?size=16" />
+      </Head>
       <body>
         <Main />
         <NextScript />
